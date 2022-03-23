4d9477bb6637712e958ffbef9df5fc79a5907aae
kevingzr-gsb <72576573+kevingzr-gsb@users.noreply.github.com>
2022-03-22 23:24:59 -0400

    Add files via upload
    Fixed all the bugs except not sure how to sort dates in csv format and how to test them, any insights would be helpful!

--- test_tasks.py
+++ test_tasks.py
@@ -1,2 +1,35 @@
-def test_tasks():
-    pass
+from tempfile import TemporaryFile
+import os
+import unittest
+import tasks
+
+def test_listing_tasks():
+
+    tasks.create("Hi", filename="tests.csv")
+    tasks.create("Hello World", filename="tests.csv")
+    with TemporaryFile("w+") as stdout:
+        tasks.list(stdout=stdout, filename="tests.csv")
+        stdout.seek(0)
+        contents = stdout.readlines()
+    os.remove("tests.csv")
+
+    assert contents == ["Hi\n", "Hello World\n"]
+
+
+def test_raise_exception_when_completed(monkeypatch):
+
+    tasks.create("Hi", filename="tests.csv")
+    tasks.create("Hello World", filename="tests.csv")
+
+    monkeypatch.setattr('builtins.input', lambda _: "1")
+
+    tasks.complete(filename="tests.csv")
+
+    with TemporaryFile("w+") as stdout:
+        tasks.complete(stdout=stdout, filename="tests.csv")
+        stdout.seek(0)
+        contents = stdout.readlines()
+
+    os.remove("tests.csv")
+
+    assert contents == ["This task has been completed, please try another one"]
